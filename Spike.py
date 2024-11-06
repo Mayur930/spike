7f2f35aff27057ed4fd5daee0c75337a6a6e0edb
Mayur930 <wmayur930@gmail.com>
2024-11-07 04:03:45 +0530

    Update Spike.py

--- Spike.py
+++ Spike.py
@@ -4,8 +4,8 @@ from telegram import Update
 from telegram.ext import Application, CommandHandler, CallbackContext
 from telegram.error import TelegramError
 
-TELEGRAM_BOT_TOKEN = '7880546265:AAj5c5PTqfJ7Lx22o7pj5MPa4ckTjuU'
-ALLOWED_USER_ID = 7372592  
+TELEGRAM_BOT_TOKEN = '7707488502:AAG-GKDSXZRKoz0U255KCQmk86liWd5krAk'
+ALLOWED_USER_ID = 1213841794
 bot_access_free = True  
 
 async def start(update: Update, context: CallbackContext):
